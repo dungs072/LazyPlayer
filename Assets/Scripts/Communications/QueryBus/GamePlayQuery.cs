61527b03c620fb40b46b8708a7d911d20c5879e9
dungs072 <82323846+dungs072@users.noreply.github.com>
2026-03-08 01:15:21 +0700

    update game screen animation

--- Assets/Scripts/Communications/QueryBus/GamePlayQuery.cs
+++ Assets/Scripts/Communications/QueryBus/GamePlayQuery.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
 public struct GetCenterCameraPositionQuery { }
+
 public struct GetEntityQuery
 {
     public string entityName;
     public Vector3 position;
 }
+
 public struct GetSnapGridPositionQuery
 {
     public Vector3 position;
---
+++ Assets/Scripts/Screens/GameScreen/Elements/MenuGamePlayPanel.cs
@@ -0,0 +1,131 @@
+using BaseEngine;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class MenuGamePlayPanel : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    [SerializeField]
+    private RectTransform tab1;
+
+    [SerializeField]
+    private RectTransform tab2;
+
+    [SerializeField]
+    private RectTransform tab3;
+
+    [SerializeField]
+    private MagicButton manageButton;
+
+    [SerializeField]
+    private MagicButton shopButton;
+
+    [SerializeField]
+    private MagicButton galleryButton;
+
+    [SerializeField]
+    private MagicButton settingButton;
+
+    [SerializeField]
+    private MagicButton closeButton;
+    private float originalWidth;
+
+    void Awake()
+    {
+        originalWidth = rectTransform.rect.width;
+        SubcribeEvents();
+        CloseTabsMenu();
+    }
+
+    private void SubcribeEvents()
+    {
+        manageButton.AddListener(HandleManageButtonClicked);
+        shopButton.AddListener(HandleShopButtonClicked);
+        galleryButton.AddListener(HandleGalleryButtonClicked);
+        settingButton.AddListener(HandleSettingButtonClicked);
+        closeButton.AddListener(HandleCloseButtonClicked);
+    }
+
+    void OnDestroy()
+    {
+        manageButton.RemoveListener(HandleManageButtonClicked);
+        shopButton.RemoveListener(HandleShopButtonClicked);
+        galleryButton.RemoveListener(HandleGalleryButtonClicked);
+        settingButton.RemoveListener(HandleSettingButtonClicked);
+        closeButton.RemoveListener(HandleCloseButtonClicked);
+    }
+
+    private void CloseTabsMenu()
+    {
+        var tab1Width = tab1.rect.width;
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, tab1Width);
+    }
+
+    private async UniTask HandleManageButtonClicked()
+    {
+        GamePlugin.BlockInput(true);
+        await OpenTabsMenuAsync();
+        GamePlugin.BlockInput(false);
+    }
+
+    private async UniTask HandleShopButtonClicked()
+    {
+        GamePlugin.BlockInput(true);
+        await OpenTabsMenuAsync();
+        GamePlugin.BlockInput(false);
+    }
+
+    private async UniTask HandleGalleryButtonClicked()
+    {
+        GamePlugin.BlockInput(true);
+        await OpenTabsMenuAsync();
+        GamePlugin.BlockInput(false);
+    }
+
+    private async UniTask HandleSettingButtonClicked()
+    {
+        GamePlugin.BlockInput(true);
+        await OpenTabsMenuAsync();
+        GamePlugin.BlockInput(false);
+    }
+
+    private async UniTask OpenTabsMenuAsync()
+    {
+        var seq = DOTween.Sequence();
+
+        float h = rectTransform.sizeDelta.y;
+
+        seq.Append(
+            rectTransform
+                .DOSizeDelta(new Vector2(originalWidth + 35, h), 0.28f)
+                .SetEase(Ease.OutCubic)
+        );
+
+        seq.Append(
+            rectTransform
+                .DOSizeDelta(new Vector2(originalWidth - 10, h), 0.12f)
+                .SetEase(Ease.InOutQuad)
+        );
+
+        seq.Append(
+            rectTransform.DOSizeDelta(new Vector2(originalWidth, h), 0.10f).SetEase(Ease.OutQuad)
+        );
+
+        await seq.AsyncWaitForCompletion();
+    }
+
+    private async UniTask HandleCloseButtonClicked()
+    {
+        GamePlugin.BlockInput(true);
+
+        await rectTransform
+            .DOSizeDelta(new Vector2(tab1.rect.width, rectTransform.sizeDelta.y), 0.35f)
+            .SetEase(Ease.InQuart)
+            .AsyncWaitForCompletion();
+
+        GamePlugin.BlockInput(false);
+    }
+}
