4ceb41d7c7735877404711433b529fe6ba235924
dungs072 <82323846+dungs072@users.noreply.github.com>
2026-03-07 21:03:57 +0700

    hot fix null ref in movement (hot-fix)

--- Assets/Scripts/GamePlay/GameObjects/Character/Actions/Movement.cs
+++ Assets/Scripts/GamePlay/GameObjects/Character/Actions/Movement.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    
+    [SerializeField]
+    private float speed = 5f;
+
     public async UniTask Move(CancellationToken cancellationToken, Vector3 position)
     {
-        while (transform.position != position)
+        //TODO: Have to fix this
+        while (this != null && transform != null && transform.position != position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
-            await UniTask.NextFrame(PlayerLoopTiming.Update,cancellationToken);
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                position,
+                Time.deltaTime * speed
+            );
+            await UniTask.NextFrame(PlayerLoopTiming.Update, cancellationToken);
         }
-    }   
+    }
 }
